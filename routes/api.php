b305d4eca048e0cd12643190654c8d44129b2e9b
kris4765 <kris.vexma@gmail.com>
2025-09-25 10:50:23 +0530

    Guide in api

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,48 @@ Route::middleware('auth:sanctum')->group(function () {
     // Logout
     Route::post('/logout', [AuthController::class, 'logout']);
 });
+
+
+/*
+Json data for testing
+
+/api/register
+{
+  "name": "krish virani",
+  "email": "kris@example.com",
+  "password": "abcd"
+}
+
+/api/Login
+{
+  "email": "kris@example.com",
+  "password": "abcd"
+}   --> provides Token
+
+
+
+
+api/stores
+{
+  "name": "My s2 Store"
+}
+
+
+api/store-setup/business-details
+{
+  "store_id": 1,
+  "business_name": "John's Store",
+  "owner_name": "John Doe",
+  "email": "john.store@example.com",
+  "phone": "9876543210",
+  "gst_number": "27ABCDE1234F2Z5",
+  "address": "123 Main Street, City, Country"
+}
+
+
+GET :: /api/store-setup/business-details/
+[All stores and their business profiles for current loggged in user]
+
+
+delete :: /api/store-setup/business-details/{id}
+*/
